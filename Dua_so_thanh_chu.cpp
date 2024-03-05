e8ca1684919d2b6dbfe6f611cad86d86e79761eb
BLACING <153001883+BLACING@users.noreply.github.com>
2024-03-05 20:50:55 +0700

    Add files via upload

---
+++ Dua_so_thanh_chu.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+using namespace std;
+int main(){
+	int n;
+	cin >>n;
+	if(n==0) cout <<"Khong\n"; 
+	else if(n==1) cout <<"Mot\n";
+	else if(n==2) cout <<"Hai\n";
+	else if(n==3) cout <<"Ba\n";
+	else if(n==4) cout <<"Bon\n";
+	else if(n==5) cout <<"Nam\n";
+	else if(n==6) cout <<"Sau\n";
+	else if(n==7) cout <<"Bay\n";
+	else if(n==8) cout <<"Tam\n";
+	else if(n==9) cout <<"Chin\n";
+	else cout <<"Nhap sai\n";
+} 
